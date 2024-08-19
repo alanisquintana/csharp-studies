e1698c25f0d129d8cf6d8f0bc68fcf3472cf2b64
Alanis Quintana <alanisquintana.dev@gmail.com>
2024-08-19 16:18:49 -0300

    chore: modify some files

--- lessons/class-example/class-example/Program.cs
+++ lessons/class-example/class-example/Program.cs
@@ -2,9 +2,9 @@
 {
     static void Main()
     {
-        Customer c1, c2;
-        c1 = new Customer("001", "Jane");
-        c2 = new Customer("002", "John");
+        
+        var c1 = new Customer("001", "Jane");
+        var c2 = new Customer("002", "John");
 
         Console.WriteLine($"Costumer Id: {c1.customerId}\nCostumer Name: {c1.customerName}");
         Console.WriteLine($"Costumer Id: {c2.customerId}\nCostumer Name: {c2.customerName}");
