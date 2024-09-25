8b762711cecedbbfa302490944ba443ce0ac0fff
Alanis Quintana <alanisquintana.dev@gmail.com>
2024-09-25 16:58:30 -0300

    feat: add code from methods lesson

---
+++ lessons/methods-example/ClassLibrary/Classes.cs
@@ -0,0 +1,47 @@
+﻿public class Product
+{
+    public static List<Product> products = [];
+
+    public string productId;
+    public string productName;
+    public double cost;
+    public double tax;
+    public int quantityInStock;
+    public static int totalNoProducts = 0;
+    public const string categoryName = "Electronics";
+    public readonly string dateOfPurchase;
+
+    public Product(string productId, string productName, double cost, int quantityInStock)
+    {
+        this.productId = productId;
+        this.productName = productName;
+        this.cost = cost;
+        this.quantityInStock = quantityInStock;
+        this.dateOfPurchase = DateTime.Now.ToShortDateString();
+        CalculateTax();
+
+        products.Add(this);
+
+        totalNoProducts++;
+    }
+
+    public static int TotalQuantity => products.Sum(p => p.quantityInStock);
+
+    public static Product? MostExpensiveProduct => products.OrderByDescending(p => p.cost).FirstOrDefault();
+
+    public override string ToString() => 
+        $"\nID: {productId}\nName: {productName}" +
+        $"\nCost: $ {cost:F2}" +
+        $"\nQuantity in Stock: {quantityInStock}" +
+        $"\nDate of Purchase: {this.dateOfPurchase}" +
+        $"\nTax: $ {tax:F2}";
+
+    public double CalculateTax()
+    {
+        tax = cost > 2000 ? cost * 12.5 / 100 : cost * 10 / 100;
+
+        return tax;
+    }
+}
+
+
---
+++ lessons/methods-example/Program.cs
@@ -0,0 +1,42 @@
+﻿class Sample
+{
+    static void Main()
+    {
+        const string developerName = "Alanis Quintana";
+
+        Product product1 = new("001", "Mobile", 1800.00, 120);
+        Product product2 = new("002", "Laptop", 2600.90, 80);
+        Product product3 = new("003", "Speakers", 80.25, 60);
+
+        if (!Product.products.Any())
+        {
+            Console.WriteLine("No products available.");
+
+            Console.ReadKey();
+
+            return;
+        }
+
+        Console.WriteLine($"\nCategory of Products: {Product.categoryName}");
+
+        foreach (var product in Product.products)
+        {
+            Console.WriteLine(product);
+        }
+
+        Console.WriteLine("\n-----------------------------------");
+
+        Console.WriteLine($"\nTotal Quantity in Stock: {Product.TotalQuantity}");
+
+        Console.WriteLine($"\nTotal Number of Products: {Product.totalNoProducts}");
+
+        Console.WriteLine($"\nMost Expensive Product:\n {Product.MostExpensiveProduct}");
+
+        Console.WriteLine("\n-----------------------------------");
+
+        Console.WriteLine($"Developer: {developerName}");
+
+        Console.ReadKey();
+    }
+}
+
